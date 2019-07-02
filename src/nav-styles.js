2252c631ffa07351c144aff38ed67a773aae1628
wil.i.am <wiliam.tae@gmail.com>
2019-07-01 21:37:02 -0300

    slider nav style added

--- src/nav-styles.js
+++ src/nav-styles.js
@@ -1,5 +1,30 @@
 import config from './config-styles.js'
 
+const btn = {
+  flex: 1,
+  height: config.imageHeight,
+  padding: 20,
+  fontSize: 50,
+  background: 'transparent',
+  color: '#fff',
+  textShadow: '1px 1px 3px rgba(0, 0, 0, 0.6)',
+  border: 'none',
+  outline: 0,
+  cursor: 'pointer',
+  userSelect: 'none'
+
+}
+
+const prev = {
+  ...btn,
+  textAlign: 'left'
+}
+
+const next = {
+  ...btn,
+  textAlign: 'right'
+}
+
 export default {
   root: {
     position: 'absolute',
@@ -8,6 +33,6 @@ export default {
     width: '100%',
     display: 'flex'
   },
-  prev: {},
-  next: {}
+  prev,
+  next
 }
